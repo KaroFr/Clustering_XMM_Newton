f03523537fef6b313c72a7514aa2999b695162d4
Karolin Frohnapfel <Karolin.Frohnapfel@ext.esa.int>
2022-08-18 09:57:44 +0200

    edit results of multi threading

--- Multithreading.py
+++ Multithreading.py
@@ -4,7 +4,9 @@ Created on Tue Aug 16 10:24:56 CEST 2022
 
 @author: jvperea
 """
-from concurrent.futures import ThreadPoolExecutor, wait, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from astropy.table import Table
+from astropy.table import vstack
 
 """
 Class for multi-threading processing
@@ -28,6 +30,7 @@ class Multithreading:
         input_list = self.input_list
         kwargs = self.kwargs
 
+        final_result = Table()
         print(f'\t- Start parallel run ... threads = {threads}')
         with ThreadPoolExecutor(max_workers=threads) as executor:
             future_to_work = {executor.submit(fn, input=input_list[i], **kwargs): i for i
@@ -37,10 +40,12 @@ class Multithreading:
                 i = future_to_work[future]
                 try:
                     d = future.result()
+                    final_result = vstack([final_result, Table(d)])
                 except Exception as exc:
                     print(f'\t{i} generated an exception: {exc}')
                 else:
                     print(f'\t- Result for thread {i} : ok')
+        return final_result
 
 
 
